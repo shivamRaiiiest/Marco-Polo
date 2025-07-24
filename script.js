41b2daad3583b94771904718f7dabdb4445bc0aa
unknown <shivamrai27072@gmail.com>
2025-07-24 12:07:38 +0530

    The website is now more responsive.

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 (function ($) {
     "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-
 
     // Initiate the wowjs
     new WOW().init();
@@ -25,47 +15,16 @@
         }
     });
 
-
-    // Dropdown on mouse hover
-    const $dropdown = $(".dropdown");
-    const $dropdownToggle = $(".dropdown-toggle");
-    const $dropdownMenu = $(".dropdown-menu");
-    const showClass = "show";
-
-    $(window).on("load resize", function () {
-        if (this.matchMedia("(min-width: 992px)").matches) {
-            $dropdown.hover(
-                function () {
-                    const $this = $(this);
-                    $this.addClass(showClass);
-                    $this.find($dropdownToggle).attr("aria-expanded", "true");
-                    $this.find($dropdownMenu).addClass(showClass);
-                },
-                function () {
-                    const $this = $(this);
-                    $this.removeClass(showClass);
-                    $this.find($dropdownToggle).attr("aria-expanded", "false");
-                    $this.find($dropdownMenu).removeClass(showClass);
-                }
-            );
-        } else {
-            $dropdown.off("mouseenter mouseleave");
-        }
-    });
-
-
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
             $('.back-to-top').fadeIn('slow');
         } else {
-            $('.back-to-top').fadeOut('slow');
+            $('.back-to-top').fadeOut('fast');
         }
     });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({ scrollTop: 0 }, 100, 'easeInOutExpo');
-        return false;
-    });
+
+    $(window).trigger('scroll');
 
 
     // Testimonials carousel
@@ -76,7 +35,7 @@
         margin: 24,
         dots: true,
         loop: true,
-        nav: false,
+        nav: true,
         responsive: {
             0: {
                 items: 1
